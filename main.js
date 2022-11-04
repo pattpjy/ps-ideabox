46b67467caeda2702e455c873cd6d3ff85bfe6fb
Timothy Pendarvis <trpendarvis@gmail.com>
2022-11-03 21:39:40 -0600

    Add functions and push method to save title and body to an array

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-var ideas = []
-
 //querySelector variables
-var newTitle = document.querySelector(addHTML id)
-var newBody = document.querySelector()
-var saveIdeaButton = document.querySelector()
-var cardBox = document.querySelector()
+var newTitle = document.querySelector("#titleInput")
+var newBody = document.querySelector("#bodyInput")
+var saveIdeaButton = document.querySelector("#saveButton")
+var showStarredIdeasButton = document.querySelector("#showStarredIdeasButton")
+// var cardBox = document.querySelector() //what is this for?
+
 
 //eventListeners
-newTitle.addEventListener("input", somefunction)
-newBody.addEventListener("input", somefunction)
-saveIdeaButton.addEventListener("click", somefunction)
-cardBox.addEventListener("click", somefunction)
-*cardBox.addEventListener("click", somefunctionSave) //might need to change name for star button
-*cardBox.addEventListener("click", somefunctionDelete) //might need to change name for X button
-showStarredIdeasButton.addEventListener("click", somefunction)
+// newTitle.addEventListener("input", somefunction)
+// newBody.addEventListener("input", somefunction)
+saveIdeaButton.addEventListener("click", function(event) {
+    saveIdeas() //save to array data model
+    //another function to display to DOM like function displayUserIdeaCard
+    event.preventDefault()
+})
+// cardBox.addEventListener("click", somefunction)
+// *cardBoxSave.addEventListener("click", somefunctionSave) //might need to change name for star button
+// *cardBoxDelete.addEventListener("click", somefunctionDelete) //might need to change name for X button
+// // showStarredIdeasButton.addEventListener("click", somefunction)
+var ideas = []
+// // userIdeaBox.classList.toggle('hidden') this will un-hidden
 
+function saveIdeas(){
+    console.log("hello")
+    var newIdea = new Idea(newTitle.value, newBody.value)
+    ideas.push(newIdea)
+    resetForm()
+}
+
+function resetForm(){
+    newTitle.value = ""
+    newBody.value = ""
+}
