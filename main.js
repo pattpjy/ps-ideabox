f9692015e45fab39aa5497edb235ea6e65308303
Timothy Pendarvis <trpendarvis@gmail.com>
2022-11-03 22:55:17 -0600

    Add functionality to form to change DOM display of button

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ var showStarredIdeasButton = document.querySelector("#showStarredIdeasButton")
 
 
 //eventListeners
-// newTitle.addEventListener("input", somefunction)
-// newBody.addEventListener("input", somefunction)
+newTitle.addEventListener("input", showSaveButton)
+newBody.addEventListener("input", showSaveButton)
 saveIdeaButton.addEventListener("click", function(event) {
     saveIdeas() //save to array data model
+    showSaveButton()
+    hideSaveButton() // why is this affecting the function hide and save?
     //another function to display to DOM like function displayUserIdeaCard
     event.preventDefault()
 })
@@ -26,9 +28,28 @@ function saveIdeas(){
     var newIdea = new Idea(newTitle.value, newBody.value)
     ideas.push(newIdea)
     resetForm()
+    saveIdeaButton.style.opacity = "0.1"
 }
 
 function resetForm(){
     newTitle.value = ""
     newBody.value = ""
 }
+
+function showSaveButton() {
+    if(newTitle.value != "" && newBody.value != ""){
+    saveIdeaButton.style.opacity = "1"
+    saveIdeaButton.style.cursor = "pointer"
+    }
+}
+
+function hideSaveButton() {
+    if(newTitle.value === "" || newBody.value === ""){
+    saveIdeaButton.style.opacity = "0.1"
+    }
+    //function is not fully working when you fill out value for either body or title then delete one the button does not hide but it should. 
+}
+
+function makeUserIdeaCard() {
+    cardBox.innerHTML += 
+    //start of add UserCard to DOM
