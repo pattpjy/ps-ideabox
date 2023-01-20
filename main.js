8178a4b8376c5067da928dc90073e8fd095d23e7
Patt Sookmark <pijiya@sookmark.com>
2023-01-20 15:19:52 -0700

    Refactor method to dryup

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ let ideas = [];
 function saveIdeas() {
   const newIdea = new Idea(pom.newTitle.value, pom.newBody.value);
   ideas.push(newIdea);
+  makeUserIdeaCard(newIdea);
+  // controller invoke view method, give it a value and view update the dom
   resetForm();
 }
--- view.js
+++ view.js
@@ -9,7 +9,7 @@ const pom = {
   cardBox: document.querySelector(".bottom-section"),
   cardBoxDelete: document.querySelector(".bottom-section"),
 };
-
+// all of these function need events?
 pom.newTitle.addEventListener("input", function () {
   hideSaveButton();
   showSaveButton();
@@ -23,7 +23,6 @@ pom.newBody.addEventListener("input", function () {
 pom.saveIdeaButton.addEventListener("click", function (event) {
   event.preventDefault();
   saveIdeas();
-  makeUserIdeaCard();
 });
 
 pom.cardBoxDelete.addEventListener("click", function (event) {
@@ -55,23 +54,108 @@ function hideSaveButton() {
     pom.saveIdeaButton.disabled = true;
   }
 }
-function makeUserIdeaCard() {
-  pom.cardBox.innerHTML = "";
-  for (var i = 0; i < ideas.length; i++) {
-    pom.cardBox.innerHTML += `<div class="user-idea-box" id="${ideas[i].id}">
-                <div id="miniboxTop">
-                    <button class="star-button" id="starButtonInactive"></button>
-                    <button class="x-button" id="xButton"></button>
-                </div>
-                <div id="miniboxInner">
-                    <h2 class="idea-title" id="ideaTitle">${ideas[i].title}</h2>
-                    <p class="idea-body" id="ideaBody">${ideas[i].body}</p>
-                </div>
-                <div id="miniboxFooter">
-                </div>
-            </div>`;
-  }
+// function makeUserIdeaCard() {
+//   pom.cardBox.innerHTML = "";
+//   for (var i = 0; i < ideas.length; i++) {
+//     pom.cardBox.innerHTML += `<div class="user-idea-box" id="${ideas[i].id}">
+//                 <div id="miniboxTop">
+//                     <button class="star-button" id="starButtonInactive"></button>
+//                     <button class="x-button" id="xButton"></button>
+//                 </div>
+//                 <div id="miniboxInner">
+//                     <h2 class="idea-title" id="ideaTitle">${ideas[i].title}</h2>
+//                     <p class="idea-body" id="ideaBody">${ideas[i].body}</p>
+//                 </div>
+//                 <div id="miniboxFooter">
+//                 </div>
+//             </div>`;
+//   }
+// }
+
+// function makeUserIdeaCard(ideaArr) {
+//   const createBox = ideaArr.forEach((idea) => {
+//     const box = document.createElement("div");
+//     box.className = "user-idea-box";
+//     box.id = idea.id;
+
+//     const top = document.createElement("div");
+//     top.id = "miniBoxTop";
+//     const starBtn = document.createElement("button");
+//     starBtn.className = "star-button";
+//     starBtn.id = "starButtonInactive";
+//     const xBtn = document.createElement("button");
+//     xBtn.className = "x-button";
+//     xBtn.id = "xButton";
+
+//     const boxBody = document.createElement("div");
+//     boxBody.id = "miniBoxInner";
+
+//     const title = document.createElement("h2");
+//     title.className = "idea-title";
+//     title.id = "ideaTitle";
+//     title.innerText = idea.title;
+
+//     const body = document.createElement("p");
+//     body.className = "idea-body";
+//     body.id = "ideaBody";
+//     body.innerText = idea.body;
+
+//     const footer = document.createElement("div");
+//     footer.id = "miniBoxFooter";
+
+//     pom.cardBox.appendChild(box);
+
+//     box.appendChild(top);
+//     top.appendChild(starBtn);
+//     top.appendChild(xBtn);
+//     box.appendChild(boxBody);
+//     box.appendChild(footer);
+//     boxBody.appendChild(title);
+//     boxBody.appendChild(body);
+//   });
+// }
+
+function makeUserIdeaCard(newlyAdd) {
+  const box = document.createElement("div");
+  box.className = "user-idea-box";
+  box.id = newlyAdd.id;
+
+  const top = document.createElement("div");
+  top.id = "miniBoxTop";
+  const starBtn = document.createElement("button");
+  starBtn.className = "star-button";
+  starBtn.id = "starButtonInactive";
+  const xBtn = document.createElement("button");
+  xBtn.className = "x-button";
+  xBtn.id = "xButton";
+
+  const boxBody = document.createElement("div");
+  boxBody.id = "miniBoxInner";
+
+  const title = document.createElement("h2");
+  title.className = "idea-title";
+  title.id = "ideaTitle";
+  title.innerText = pom.newTitle.value;
+
+  const body = document.createElement("p");
+  body.className = "idea-body";
+  body.id = "ideaBody";
+  body.innerText = pom.newBody.value;
+
+  const footer = document.createElement("div");
+  footer.id = "miniBoxFooter";
+
+  pom.cardBox.appendChild(box);
+
+  box.appendChild(top);
+  top.appendChild(starBtn);
+  top.appendChild(xBtn);
+  box.appendChild(boxBody);
+  box.appendChild(footer);
+  boxBody.appendChild(title);
+  boxBody.appendChild(body);
 }
+
 function deleteUserCard(event) {
   if (event.target.id === "xButton") {
     for (var i = 0; i < ideas.length; i++) {
